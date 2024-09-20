d8d1b71ea899218744c8901993c010b451725a8a
ST10145185 <ST10145185@vcconnect.edu.za>
2024-09-20 11:53:32 +0200

    Login and Register frontend

---
+++ app/src/main/java/com/example/opsc7312_poe/Login.kt
@@ -0,0 +1,33 @@
+package com.example.opsc7312_poe
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_login)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.login)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        // Get the reference to the TextView
+        val signUpLink = findViewById<TextView>(R.id.signUpLink)
+
+        // Set OnClickListener to the TextView
+        signUpLink.setOnClickListener {
+            // Start RegisterActivity
+            val intent = Intent(this, Register::class.java)
+            startActivity(intent)
+        }
+    }
+
+
+}
---
+++ app/src/main/java/com/example/opsc7312_poe/Register.kt
@@ -0,0 +1,31 @@
+package com.example.opsc7312_poe
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Register : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_register)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.register)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        // Get the reference to the TextView
+        val signUpLink = findViewById<TextView>(R.id.signInLink)
+
+        // Set OnClickListener to the TextView
+        signUpLink.setOnClickListener {
+            // Start RegisterActivity
+            val intent = Intent(this, Login::class.java)
+            startActivity(intent)
+        }
+    }
+}
